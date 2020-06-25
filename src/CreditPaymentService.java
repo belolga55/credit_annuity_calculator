6efe06ec139258f1dd40eec60c418f3a133906a1
Olga Gladiy <bel-olga@yandex.ru>
2020-06-25 17:59:25 +0300

    Format code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
     public double calculate(float amount, double percent, float period) {
-        double monthpercent = percent/100/12;
-        double creditpayment = amount * (monthpercent + monthpercent/(Math.pow((1 + monthpercent), (period * 12) ) - 1));
+        double monthpercent = percent / 100 / 12;
+        double creditpayment = amount * (monthpercent + monthpercent / (Math.pow((1 + monthpercent), (period * 12)) - 1));
 
         return creditpayment;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        double creditpayment = service.calculate (1000000, 9.99, 1);
-        System.out.printf(" Eжемесячный платёж, период 1 год %.0f\n" , creditpayment);
+        double creditpayment = service.calculate(1000000, 9.99, 1);
+        System.out.printf(" Eжемесячный платёж, период 1 год %.0f\n", creditpayment);
 
-        double creditpayment1 = service.calculate (1000000, 9.99, 2);
+        double creditpayment1 = service.calculate(1000000, 9.99, 2);
         System.out.printf(" Eжемесячный платёж, период 2 года %.0f\n", creditpayment1);
 
-        double creditpayment2 = service.calculate (1000000, 9.99, 3);
+        double creditpayment2 = service.calculate(1000000, 9.99, 3);
         System.out.printf(" Eжемесячный платёж, период 3 года %.0f\n", creditpayment2);
 
     }
